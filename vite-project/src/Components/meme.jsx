9e119689ecb927e0bbc1e521a6c4c81775815ecf
brickphone <mattis.hutt@gmail.com>
2023-06-27 22:59:54 +0200

    image state

--- vite-project/src/Components/meme.jsx
+++ vite-project/src/Components/meme.jsx
@@ -1,22 +1,28 @@
+import {useState} from "react"
 import memesdata from '../memesdata'
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState("")
+  
   function getMemeImg() {
     const memesArray = memesdata.data.memes
     const randomMeme = memesArray[Math.floor(Math.random() * memesArray.length)]
-    const Url = randomMeme.url
-
-    console.log(Url)
+    setMemeImage(randomMeme.url)
+    
+    console.log(setMemeImage)
   }
 
   return (
     <main>
       <div className="form">
-        <div className="input-group">
-          <input type="text" placeholder="Top text" />
-          <input type="test" placeholder="Boottom text" />
+        <div className="meme-container">
+          <div className="input-group">
+            <input type="text" placeholder="Top text" />
+            <input type="test" placeholder="Boottom text" />
+          </div>
+            <button onClick={getMemeImg} className="meme-button">Get a new meme image🖼️</button>
         </div>
-        <button onClick={getMemeImg} className="meme-button">Get a new meme image🖼️</button>
+        <img src={memeImage} className="meme-img"></img>
       </div>
     </main>
   )
